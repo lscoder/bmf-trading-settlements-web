0de04de348c8d68eda9eeabd89193fad93be4a31
Leonardo Campos <leonardo.campos@bloomingdales.com>
2016-10-08 21:29:45 -0300

    Adding ProcFile

--- routes/contracts.js
+++ routes/contracts.js
@@ -4,8 +4,12 @@ const express = require('express'),
 
 router.get('/:contract', function(req, res, next) {
   bmf(req.params.contract).then((data) => {
-    res.send(data);
-  })
+    res.status(200).send(data);
+  }).catch(err => {
+    res.status(500).send({
+      err: err
+    });
+  });
 });
 
 module.exports = router;
